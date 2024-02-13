9746c61d9b481e5d01a1b2756f795535146b4c1f
WaltSias <walt.sias@gmail.com>
2024-02-12 21:33:47 -0600

    added buttons, changed color, and text styling

---
+++ buttons.js
@@ -0,0 +1,25 @@
+goButton1.onclick = function () {
+    alert("Not Quite");
+    goButton1.style.color = "white";
+    goButton1.style.backgroundColor = "red";
+}
+goButton2.onclick = function () {
+    alert("Not Quite");
+    goButton2.style.color = "white";
+    goButton2.style.backgroundColor = "red";
+}
+goButton3.onclick = function () {
+    alert("Not Quite");
+    goButton3.style.color = "white";
+    goButton3.style.backgroundColor = "red";
+}
+goButton4.onclick = function () {
+    alert("Winner!");
+    goButton4.style.color = "white";
+    goButton4.style.backgroundColor = "green";
+}
+goButton5.onclick = function () {
+    alert("Not Quite");
+    goButton5.style.color = "white";
+    goButton5.style.backgroundColor = "red";
+}
